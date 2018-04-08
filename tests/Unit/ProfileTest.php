1885e1083be69d55457cd9c55150d230101e0b88
lookittah <ajh59@njit.edu>
2018-04-08 19:45:35 -0400

    I have added user profile migrate, model, and updated the user seeder to create profiles.

---
+++ tests/Unit/ProfileTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use App\Profile;
+class profileTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testSave()
+    {
+        $user = factory(User::class)->make();
+        $user->save();
+        $profile = factory(Profile::class)->make();
+        $profile->user()->associate($user);
+        $this->assertTrue($profile->save());
+    }
+}
