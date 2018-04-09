c75e2412e29ad9eb5d89e82fab7266b606a511dc
lookittah <ajh59@njit.edu>
2018-04-08 21:28:12 -0400

    User Unit Test passed

--- app/User.php
+++ app/User.php
@@ -50,6 +50,10 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Profile');
     }
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
 
 
 }
