9575a64ebfe37ea232eed60cb3f5f9adfc2056a1
lookittah <ajh59@njit.edu>
2018-04-23 23:45:57 -0400

    Procfile for Heroku

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-04-22 05:59:46.
+    * Generated on 2018-04-23 00:17:25.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
