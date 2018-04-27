65489dbb81e994faccc720e61d90207616068cff
lookittah <ajh59@njit.edu>
2018-04-27 16:45:20 -0400

    I fixed the profile creation for the app

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-04-23 00:17:25.
+    * Generated on 2018-04-24 23:38:58.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
